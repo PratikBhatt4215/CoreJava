fed0458b433a9dea2bd00d5535558a2d8135e3a0
Pratikbhatt4215 <pk4789218@gmail.com>
2022-12-15 23:12:03 +0530

    Sport Complex problem solved

--- src/Q15/Basements.java
+++ src/Q15/Basements.java
@@ -1,16 +1,20 @@
 package Q15;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Basements {
 	Scanner sc = new Scanner(System.in);
-	int[] bikeid = new int[15];
+	
+	
+	HashSet<Integer> hs = new HashSet<>(15);
+	
 	
 	public void bikes() {
 
 		System.out.println("\t BASEMENT 1 FOR BIKES :");
 		
-		System.out.println("Total slot available :" +bikeid.length);
+		System.out.println("Total slot available :" +hs.size());
 		System.out.println();
 		System.out.println("Enter how many bikes slots you want");
 		
@@ -18,14 +22,17 @@ public class Basements {
 		
 		for(int i =0 ; i<=bikesl; i++) {
 			
-			bikeid[i] = sc.nextInt();
+			int bike = sc.nextInt();
+			
+			hs.add(bike);
 			
 		}
+		Integer[] array = hs.toArray( new Integer[hs.size()] );
 		for(int i =0 ;i<=bikesl;i++) {
-			System.out.println(" Id of bike is  "+ bikeid[i]+" slots booked successfully for bike type " +i);
+			System.out.println(" Id of bike is  "+ array[i]+" slots booked successfully for bike type " +i);
 		}
 		
-		System.out.println("Available slot of bikes is: " +(bikeid.length - bikesl));
+		System.out.println("Available slot of bikes is: " +(hs.size() - bikesl));
 	}
 
 }
